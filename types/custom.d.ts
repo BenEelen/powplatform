c38bfee24cf757b021d19f6efa9af88d2899a7ac
Ben Word <ben@benword.com>
2023-03-14 20:30:55 -0500

    🩹 Types fix (#1)

--- types/custom.d.ts
+++ types/custom.d.ts
@@ -4,10 +4,19 @@
  * @see {@link https://webpack.js.org/guides/typescript/#importing-other-assets}
  */
 
-import type {Alpine} from 'alpinejs';
+import type { Alpine } from 'alpinejs';
 
 declare global {
   interface Window {
     Alpine: Alpine;
+  }
+
+  const roots: {
+    register: {
+      blocks: (path: string) => void;
+      formats: (path: string) => void;
+      variations: (path: string) => void;
+      plugins: (path: string) => void;
+    }
   }
 }
