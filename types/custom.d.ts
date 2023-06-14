f94126082fcf73d873ae08b3f6fcbab22b6f5f29
Kelly Mears <developers@tinypixel.dev>
2023-06-14 10:48:12 -0400

    🧑‍💻 Bud v6.13.0 and build related improvements (#56)

--- types/custom.d.ts
+++ types/custom.d.ts
@@ -4,7 +4,15 @@
  * @see {@link https://webpack.js.org/guides/typescript/#importing-other-assets}
  */
 
-import type { Alpine } from 'alpinejs';
+import type { Alpine } from 'alpinejs'
+
+declare module "*.css"
+declare module "*.svg"
+declare module "*.jpeg"
+declare module "*.jpg"
+declare module "*.png"
+declare module "*.gif"
+declare module "*.webp"
 
 declare global {
   interface Window {
